9349ebbe05a11661b64650b0b17cd4fa4d21025d
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-01-08 16:12:14 +0200

    refactoring Filter into hooks

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from '../Filter/Filter.module.css';
 
-export class Filter extends Component {
-  handleChangeFilter = event => {
-    this.props.onFilter(event.currentTarget.value);
+export function Filter({ onFilter }) {
+  const handleChangeFilter = event => {
+    onFilter(event.currentTarget.value);
   };
-  render() {
-    return (
-      <label className={css.filterLabel}>
-        Find contacts by name
-        <input
-          className={css.filterName}
-          type="text"
-          name="filter"
-          placeholder="Search contact"
-          onChange={this.handleChangeFilter}
-        ></input>
-      </label>
-    );
-  }
+
+  return (
+    <label className={css.filterLabel}>
+      Find contacts by name
+      <input
+        className={css.filterName}
+        type="text"
+        name="filter"
+        placeholder="Search contact"
+        onChange={handleChangeFilter}
+      ></input>
+    </label>
+  );
 }
+
+// export class Filter extends Component {
+//   handleChangeFilter = event => {
+//     this.props.onFilter(event.currentTarget.value);
+//   };
+//   render() {
+//     return (
+//       <label className={css.filterLabel}>
+//         Find contacts by name
+//         <input
+//           className={css.filterName}
+//           type="text"
+//           name="filter"
+//           placeholder="Search contact"
+//           onChange={this.handleChangeFilter}
+//         ></input>
+//       </label>
+//     );
+//   }
+// }
 Filter.propTypes = {
   onFilter: PropTypes.func.isRequired,
 };
